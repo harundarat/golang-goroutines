b9c0c37a5da7dc07f8bc0d04fd31d24de414e61b
harundarat <harundarat@gmail.com>
2025-07-03 21:44:29 +0700

    create goroutine

---
+++ goroutine_test.go
@@ -0,0 +1,18 @@
+package golanggoroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Upss")
+
+	time.Sleep(1 * time.Second)
+}
