6b818ddcc643ed50151ba97a6eefc7f69c03bd19
harundarat <harundarat@gmail.com>
2025-07-05 14:51:02 +0700

    select channel

--- channel_test.go
+++ channel_test.go
@@ -107,3 +107,31 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Finish")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
