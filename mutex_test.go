6f4b2417763533430eeb36fddd8f156713dc88e9
harundarat <harundarat@gmail.com>
2025-07-07 07:26:25 +0700

    deadlock

--- mutex_test.go
+++ mutex_test.go
@@ -59,3 +59,54 @@ func TestRWMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Total Balance", account.GetBalance())
 }
+
+type UserBalance struct {
+	// Mutex sync.Mutex
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance += amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock user1", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock user2", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := UserBalance{Name: "Harun", Balance: 1000}
+	user2 := UserBalance{Name: "Rasyid", Balance: 1000}
+
+	// This will cause a deadlock because both goroutines will try to lock each other's mutex
+	// and will wait indefinitely.
+	go Transfer(&user1, &user2, 100)
+	go Transfer(&user2, &user1, 200)
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("User ", user1.Name, ", Balance ", user1.Balance)
+	fmt.Println("User ", user2.Name, ", Balance ", user2.Balance)
+}
